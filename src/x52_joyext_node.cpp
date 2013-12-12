da12f3b7933b2b10830cffe828d9f408a1699135
Murilo FM <muhrix+pluto@gmail.com>
2013-12-12 17:48:46 -0200

    Including header file within package only

---
+++ src/x52_joyext_node.cpp
@@ -0,0 +1,23 @@
+/*
+ * x52_joyext Node
+ * by Christian Holl (www.rad-lab.net)
+ * License: BSD
+ *
+ * Have Fun! :-)
+ */
+
+/*
+ * Modified by Murilo FM (muhrix@gmail.com)
+ * 12 Dec 2013
+ *
+ */
+
+#include <ros/ros.h>
+#include "x52_joyext.h"
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "X52JoyExt");
+	ros::NodeHandle n("~");
+	X52_JoyExt node(n);
+}
